98f30bbed8f2201ff2b6c39059e7166fd530098e
James Green <jgreenop22@gmail.com>
2024-07-04 12:06:50 -0700

    Block Physics by Sci_Rave updated to 1.20.1

---
+++ src/main/kotlin/net/fenrir/blockphysics/BlockphysicsDataGenerator.kt
@@ -0,0 +1,9 @@
+package net.fenrir.blockphysics
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object BlockphysicsDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
