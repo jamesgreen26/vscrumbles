fa838a3801919a374a956a91301a60dc5687bbe3
James Green <jgreenop22@gmail.com>
2024-07-04 13:11:04 -0700

    Fire physics now only on VS ships

--- src/main/java/net/fenrir/blockphysics/mixin/FireBlockMixin.java
+++ src/main/java/net/fenrir/blockphysics/mixin/FireBlockMixin.java
@@ -5,6 +5,11 @@ import net.minecraft.block.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
+
+import org.valkyrienskies.core.api.world.ServerShipWorld;
+import org.valkyrienskies.mod.common.ValkyrienSkiesMod;
+import org.valkyrienskies.mod.common.util.DimensionIdProvider;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,7 +20,11 @@ public abstract class FireBlockMixin extends BlockMixin {
 
     @Inject(method = "trySpreadingFire", at = @At("RETURN"), cancellable = true)
     protected void fireSpread(World world, BlockPos pos, int spreadFactor, Random random, int currentAge, CallbackInfo ci) {
-        if (!world.isClient()) {
+
+        ServerShipWorld serverShipWorld = (ServerShipWorld) ValkyrienSkiesMod.getVsCore().getHooks().getCurrentShipServerWorld();
+        DimensionIdProvider provider = (DimensionIdProvider) world;
+
+        if (!world.isClient() && serverShipWorld.isBlockInShipyard(pos.getX(),pos.getY(),pos.getZ(),provider.getDimensionId())) {
             if (world.getBlockState(pos).isAir()) {
                 BlockHandler.updateNeighbors(world,pos);
             }
